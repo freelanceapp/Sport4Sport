ce441c91c9a56f5f7b44dfb7499f5145097eb845
amardeep <amardeep.infobite@gmail.com>
2019-01-31 14:04:51 +0530

    mrew

--- app/src/main/java/technology/infobite/com/sportsforsports/constant/Constant.java
+++ app/src/main/java/technology/infobite/com/sportsforsports/constant/Constant.java
@@ -2,8 +2,9 @@ package technology.infobite.com.sportsforsports.constant;
 
 public class Constant {
 
-    /*http://codeencrypt.in/s4s/api/user_signup.php*/
-    public static final String BASE_URL = "http://codeencrypt.in/s4s/api/";
+    /*http://codeencrypt.in/s4s/api/user_signup.php
+     * http://codeencrypt.in/sport/images/alhlete_images/1543842695blog-7.jpg*/
+    public static final String BASE_URL = "http://codeencrypt.in/sport/admin/api/";
     public static final String LOGIN_API = "user-login.php";
     public static final String REGISTRATION_API = "user_signup.php";
     public static final String UPDATE_PROFILE_API = "update_profile.php";
@@ -31,5 +32,6 @@ public class Constant {
     public static final String USER_ID = "USER_ID";
 
     public static final String DEMO_URL = "https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=95feff9366&attid=0.1&permmsgid=msg-f:1621985822895568602&th=168273832468f6da&view=att&disp=inline&realattid=f_jqm0aavt0&saddbat=ANGjdJ9UC0MXfarPlwbD-Io9fL4D8eEKiWZsX-sDtRfg-rg8HU5kBm07vLciLg_aKeuiGWSnTq_dD-Vhpd7-SRKz3zUO9NKwnAlc-DnpLM_6MI2gBKhBvCxLeRWNvq1AlW_OOYdkx543UrZ79cmjob_hq32eXqIZo57__2Mtlx7qSaQbFhzLJBYCvEEM9_RaPiYAUt0x5FJveljJ8DyGFIFVIkx1W6ihjm_sD1oPBobRhMYYd7i_a9CWU6QzG-4tlNyTvVsEOIUIA2pQVbn3-TJPivFIcJnM0KxmT2rMs68bMGQy3Ulg49kSzGG_KaqbtUcC-1DJ0W6IKfQlhl_5fHSZtuqWEIwYJeb6f69gnK3gFRLvaJLpJXJ4_mp_IMzkZaz5pN8gqU3gFq1nfmTItpJX2ilSi4-5VpQKPs8GHrIiYRvtA4xNQY8lfTH_iVn4WENVmkZ-bVGuu8lvdEavzn3KDhC6qb_B5QB46mYGDh37snNTxB68ZaX1XKLXf8dAhTjfwVbuPUDuJ9-fEjRLqq-LE7nwBHZ3ZPnNoPcEe0geexo19zo4SyOHzJRFHDc5TYjY97e14_gPH_6PW8xgut2kMY96f_PUkd2jeuxAUinZFnBGudhTvTKL10VayNRdbVQ3OqpBnro8IrpnXvBO";
+    public static final String IMAGE_BASE_URL = "http://codeencrypt.in/sport/images/alhlete_images/";
     public static final String VIDEO_URL = "https://videocdn.bodybuilding.com/video/mp4/62000/62792m.mp4";
 }
